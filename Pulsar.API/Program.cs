9623b77f6390874959ed87a13ab91d372bda740c
Gabriel Leite <tech.gabrielleite@gmail.com>
2026-05-21 11:03:56 -0300

    feat(config): registrar todos os serviços no DI container
    - HttpClient nomeado "openweathermap" com BaseUrl e timeout de 10s
    - Repositórios Scoped: ISubprefeituraRepository, ILeituraRepository,
      IScoreRepository, ISugestaoRepository, IAlertaRepository
    - Serviços Scoped: IClimateService, IScoreService, ISugestaoService,
      IAlertaService
    - IWeatherClient → OpenWeatherMapClient (Scoped)
    - AddHostedService<DataCollectionJob>

--- Pulsar.API/Program.cs
+++ Pulsar.API/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Pulsar.API.External.Clients;
+using Pulsar.API.External.Interfaces;
 using Pulsar.API.Repositories.Data;
+using Pulsar.API.Repositories.Interfaces;
+using Pulsar.API.Scheduler;
+using Pulsar.API.Services;
+using Pulsar.API.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +14,31 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<PulsarDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient("openweathermap", (sp, client) =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    client.BaseAddress = new Uri(config["OpenWeatherMap:BaseUrl"]
+        ?? "https://api.openweathermap.org/data/2.5/");
+    if (!client.BaseAddress.AbsoluteUri.EndsWith('/'))
+        client.BaseAddress = new Uri(client.BaseAddress.AbsoluteUri + "/");
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
+
+builder.Services.AddScoped<IWeatherClient, OpenWeatherMapClient>();
+
+builder.Services.AddScoped<ISubprefeituraRepository, SubprefeituraRepository>();
+builder.Services.AddScoped<ILeituraRepository, LeituraRepository>();
+builder.Services.AddScoped<IScoreRepository, ScoreRepository>();
+builder.Services.AddScoped<ISugestaoRepository, SugestaoRepository>();
+builder.Services.AddScoped<IAlertaRepository, AlertaRepository>();
+
+builder.Services.AddScoped<IClimateService, ClimateService>();
+builder.Services.AddScoped<IScoreService, ScoreService>();
+builder.Services.AddScoped<ISugestaoService, SugestaoService>();
+builder.Services.AddScoped<IAlertaService, AlertaService>();
+
+builder.Services.AddHostedService<DataCollectionJob>();
 
 var app = builder.Build();
 

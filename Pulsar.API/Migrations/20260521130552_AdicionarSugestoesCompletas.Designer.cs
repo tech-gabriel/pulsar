a6f9581d5a6f957263fd73bf970da3d2a130f25e
Gabriel Leite <tech.gabrielleite@gmail.com>
2026-05-21 10:06:32 -0300

    feat(db): expandir catálogo de sugestões para 45 entradas
    - 3 sugestões por combinação de categoria × faixa (5 × 3 × 3)
    - Cobre CHUVA, VENTO, NEBLINA, UV e GERAL em todos os níveis
    - Migration AdicionarSugestoesCompletas aplicada

--- Pulsar.API/Migrations/20260521130552_AdicionarSugestoesCompletas.Designer.cs
+++ Pulsar.API/Migrations/20260521130552_AdicionarSugestoesCompletas.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pulsar.API.Repositories.Data;
 
@@ -10,9 +11,11 @@ using Pulsar.API.Repositories.Data;
 namespace Pulsar.API.Migrations
 {
     [DbContext(typeof(PulsarDbContext))]
-    partial class PulsarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260521130552_AdicionarSugestoesCompletas")]
+    partial class AdicionarSugestoesCompletas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
